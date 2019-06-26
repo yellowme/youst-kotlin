28bea46403c9f24d74f882a360f0ac3aad7ef618
Cesar Gomez <gomezcarrillocesar@gmail.com>
2019-06-26 15:21:16 -0500

    Add logo animation to splash screen

--- app/src/main/java/mx/yellowme/youst/presentation/splash/SplashActivity.kt
+++ app/src/main/java/mx/yellowme/youst/presentation/splash/SplashActivity.kt
@@ -1,22 +1,28 @@
 package mx.yellowme.youst.presentation.splash
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 import mx.yellowme.youst.R
+import mx.yellowme.youst.common.utils.setListener
 import mx.yellowme.youst.presentation.showcase.ShowcaseActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.SharedAppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Starts the activity after animations
-        val intent = Intent(this, ShowcaseActivity::class.java)
-        startActivity(intent)
-        finish()
+        splashAnimation?.apply {
+            useHardwareAcceleration()
+            setListener {
+                onAnimationEnd {
+                    val intent = Intent(this@SplashActivity, ShowcaseActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
     }
 }
