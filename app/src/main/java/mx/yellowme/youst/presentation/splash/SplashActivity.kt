465f237fedc575b09f87d64ab97634a2338f833c
Cesar Gomez <gomezcarrillocesar@gmail.com>
2019-06-26 17:59:52 -0500

    Revert view to synthetic

--- app/src/main/java/mx/yellowme/youst/presentation/splash/SplashActivity.kt
+++ app/src/main/java/mx/yellowme/youst/presentation/splash/SplashActivity.kt
@@ -1,7 +1,7 @@
 package mx.yellowme.youst.presentation.splash
 
 import android.content.Intent
-import com.airbnb.lottie.LottieAnimationView
+import kotlinx.android.synthetic.main.activity_splash.*
 import mx.yellowme.youst.R
 import mx.yellowme.youst.common.activities.BaseActivity
 import mx.yellowme.youst.common.utils.setListener
@@ -9,13 +9,12 @@ import mx.yellowme.youst.presentation.showcase.ShowcaseActivity
 
 class SplashActivity : BaseActivity() {
 
-    private lateinit var splashAnimationView: LottieAnimationView
-
     override val layoutResource: Int
         get() = R.layout.activity_splash
 
+    //TODO: Remove method from parent class
     override fun bindViews() {
-        splashAnimationView = findViewById<LottieAnimationView>(R.id.splashAnimationView).apply {
+        splashAnimationView?.apply {
             useHardwareAcceleration()
             setListener {
                 onAnimationEnd {
