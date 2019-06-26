07a04f66d03b2ba482fee4fd38e4b2454a85166c
Cesar Gomez <gomezcarrillocesar@gmail.com>
2019-06-26 17:34:56 -0500

    Implement base activity in splash screen

--- app/src/main/java/mx/yellowme/youst/presentation/splash/SplashActivity.kt
+++ app/src/main/java/mx/yellowme/youst/presentation/splash/SplashActivity.kt
@@ -1,19 +1,21 @@
 package mx.yellowme.youst.presentation.splash
 
 import android.content.Intent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_splash.*
+import com.airbnb.lottie.LottieAnimationView
 import mx.yellowme.youst.R
+import mx.yellowme.youst.common.activities.BaseActivity
 import mx.yellowme.youst.common.utils.setListener
 import mx.yellowme.youst.presentation.showcase.ShowcaseActivity
 
-class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+class SplashActivity : BaseActivity() {
 
-        splashAnimationView?.apply {
+    private lateinit var splashAnimationView: LottieAnimationView
+
+    override val layoutResource: Int
+        get() = R.layout.activity_splash
+
+    override fun bindViews() {
+        splashAnimationView = findViewById<LottieAnimationView>(R.id.splashAnimationView).apply {
             useHardwareAcceleration()
             setListener {
                 onAnimationEnd {
@@ -24,4 +26,5 @@ class SplashActivity : AppCompatActivity() {
             }
         }
     }
+
 }
