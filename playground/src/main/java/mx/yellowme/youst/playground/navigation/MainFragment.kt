901ac7a08328ce37b4340ffdc07e11a95d4f2e08
Cesar Gomez <cesar@yellowme.mx>
2019-08-31 14:53:23 -0500

    Change jetpack showcase screens

---
+++ playground/src/main/java/mx/yellowme/youst/playground/navigation/MainFragment.kt
@@ -0,0 +1,31 @@
+package mx.yellowme.youst.playground.navigation
+
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.screen_login.*
+import mx.yellowme.youst.core.hooks.BaseFragment
+import mx.yellowme.youst.playground.R
+import mx.yellowme.youst.playground.components.AppHeroActionListener
+
+class MainFragment : BaseFragment() {
+
+    //region Attributes
+
+    override val layoutId: Int = R.layout.screen_main
+
+    //endregion
+
+    override fun setup() {
+        appHero.message = getString(
+            R.string.screen_main,
+            getString(R.string.userName),
+            getString(R.string.paymentData)
+        )
+
+        appHero.listener = object : AppHeroActionListener {
+            override fun onClickAction() {
+                val action = MainFragmentDirections.toUnsupported()
+                findNavController().navigate(action)
+            }
+        }
+    }
+}
---
+++ playground/src/main/java/mx/yellowme/youst/playground/navigation/UserWithoutAttributeFragment.kt
@@ -0,0 +1,22 @@
+package mx.yellowme.youst.playground.navigation
+
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.screen_user_no_attribute.*
+import mx.yellowme.youst.core.hooks.BaseFragment
+import mx.yellowme.youst.playground.R
+import mx.yellowme.youst.playground.components.AppHeroActionListener
+
+class UserWithoutAttributeFragment : BaseFragment() {
+
+    override val layoutId: Int = R.layout.screen_user_no_attribute
+
+    override fun setup() {
+        appHero.listener = object : AppHeroActionListener {
+            override fun onClickAction() {
+                val action = UserWithoutAttributeFragmentDirections.toPayment()
+                findNavController().navigate(action)
+            }
+        }
+    }
+
+}
