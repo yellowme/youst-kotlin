47269e0c9bf0e8c27713033217216c166c8dc250
Cesar Gomez <cesar@yellowme.mx>
2019-08-31 12:13:02 -0500

    Create a base fragment class

---
+++ core/src/main/java/mx/yellowme/youst/core/hooks/BaseFragment.kt
@@ -0,0 +1,33 @@
+package mx.yellowme.youst.core.hooks
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+    abstract val layoutId: Int
+
+    //region Lifecycle
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutId, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setup()
+    }
+
+    //endregion
+
+    abstract fun setup()
+
+}
