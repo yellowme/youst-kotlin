9ae8709f9b4d5db31584b58717ae43da166c451b
Cesar Gomez <cesar@yellowme.mx>
2019-09-02 00:56:38 -0500

    Change website intent to email intent in contact us mini app

--- core/src/main/java/mx/yellowme/youst/core/utils/Utils.kt
+++ core/src/main/java/mx/yellowme/youst/core/utils/Utils.kt
@@ -1,14 +1,18 @@
 package mx.yellowme.youst.core.utils
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.util.TypedValue
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types.newParameterizedType
+import mx.yellowme.youst.core.extensions.launch
 import java.io.IOException
 import java.util.*
+import kotlin.collections.ArrayList
 
 //region AppCompatActivity
 
@@ -35,12 +39,23 @@ fun Activity.readJson(named: String): String? {
     return classLoader?.readJson(named)
 }
 
-inline fun <reified T : Any> Activity.loadJsonArray(named: String) : List<T>? {
+inline fun <reified T : Any> Activity.loadJsonArray(named: String): List<T>? {
     return readJson(named)?.asJsonArrayOf()
 }
 
-inline fun <reified T : Any> Activity.loadJsonObject(named: String) : T? {
+inline fun <reified T : Any> Activity.loadJsonObject(named: String): T? {
     return readJson(named)?.asJsonObject()
+}
+
+fun Activity.composeEmail(address: String, subject: String) {
+    val intent = Intent(Intent.ACTION_SENDTO).apply {
+        data = Uri.parse("mailto:")
+        val addressArray = ArrayList<String>(1)
+        addressArray.add(address)
+        putExtra(Intent.EXTRA_EMAIL, addressArray.toTypedArray())
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+    }
+    launch(intent)
 }
 
 //endregion
