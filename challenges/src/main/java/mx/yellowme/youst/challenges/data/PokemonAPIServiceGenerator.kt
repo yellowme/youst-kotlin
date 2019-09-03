2e908e5a3204e80c0c519d7ae273a512d8e44dab
Luis Burgos <luis@yellowme.mx>
2019-09-03 15:01:42 -0500

    [Challenges] Remove API server url. Closes #24

--- challenges/src/main/java/mx/yellowme/youst/challenges/data/PokemonAPIServiceGenerator.kt
+++ challenges/src/main/java/mx/yellowme/youst/challenges/data/PokemonAPIServiceGenerator.kt
@@ -1,6 +1,7 @@
 package mx.yellowme.youst.challenges.data
 
+import mx.yellowme.youst.challenges.BuildConfig
 import mx.yellowme.youst.core.data.BaseAPIServiceGenerator
 
 //TODO: Extract String URL
-class PokemonAPIServiceGenerator: BaseAPIServiceGenerator("https://ios-course-poke-api.herokuapp.com/")
+class PokemonAPIServiceGenerator: BaseAPIServiceGenerator(BuildConfig.POKEMON_API_URL)
