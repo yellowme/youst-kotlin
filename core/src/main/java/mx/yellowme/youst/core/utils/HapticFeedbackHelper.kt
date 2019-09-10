893a7aa3bed6d3df62aac6ac60bcf63b32fd49c5
kevin gamboa <kevin-gamboa1@hotmail.com>
2019-09-10 12:47:43 -0500

    Add the haptic feedback when change the chord

---
+++ core/src/main/java/mx/yellowme/youst/core/utils/HapticFeedbackHelper.kt
@@ -0,0 +1,29 @@
+package mx.yellowme.youst.core.utils
+
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+
+object HapticFeedbackHelper {
+
+    private const val DEFAULT_MILLS = 3L
+    private const val DEVICES_BELOW_API_26_MILLS = 15L
+
+    fun generateTouchFeedbackOn(context: Context) {
+        val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        if (vibrator.hasVibrator().not()) return
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val effect = VibrationEffect.createOneShot(
+                DEFAULT_MILLS,
+                VibrationEffect.DEFAULT_AMPLITUDE
+            )
+            vibrator.vibrate(effect)
+            return
+        }
+
+        vibrator.vibrate(DEVICES_BELOW_API_26_MILLS)
+    }
+}
