2bdf3e87b2fd13c2f08340c5e44050097d5f0c99
Adrián Leyva S <adrian@yellowme.mx>
2019-10-01 00:27:55 -0500

    Add chart selector component

--- playground/src/main/java/mx/yellowme/youst/playground/domain/ChartType.kt
+++ playground/src/main/java/mx/yellowme/youst/playground/domain/ChartType.kt
@@ -7,22 +7,8 @@ enum class ChartType(name: String) {
     BAR("Bar"),
     LINE("Line"),
     BUBBLE( "Bubble"),
-    UNDEFINED("undefined")
 }
 
 fun getTypeByName(name: String): ChartType {
-    return when (name) {
-        ChartType.BAR.name -> {
-            ChartType.BAR
-        }
-        ChartType.LINE.name -> {
-            ChartType.LINE
-        }
-        ChartType.BUBBLE.name -> {
-            ChartType.BUBBLE
-        }
-        else -> {
-            ChartType.UNDEFINED
-        }
-    }
+    return ChartType.valueOf(name)
 }
--- playground/src/main/java/mx/yellowme/youst/playground/ui/chart/screens/ChartViewActivity.kt
+++ playground/src/main/java/mx/yellowme/youst/playground/ui/chart/screens/ChartViewActivity.kt
@@ -1,21 +1,27 @@
 package mx.yellowme.youst.playground.ui.chart.screens
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.component_chart_selector.view.*
 import kotlinx.android.synthetic.main.screen_chart_view.*
+import mx.yellowme.youst.playground.R
 import mx.yellowme.youst.playground.domain.ChartType
 import mx.yellowme.youst.playground.domain.getTypeByName
-import mx.yellowme.youst.playground.ui.chart.common.BaseChart
+import mx.yellowme.youst.playground.ui.chart.common.BaseChartActivity
 import mx.yellowme.youst.playground.ui.chart.common.OnChangeListener
+import mx.yellowme.youst.playground.ui.chart.components.ChartSelectorActionListener
 
 /**
  * Created by adrianleyvasanchez on 29,September,2019
  */
-class ChartViewActivity : BaseChart() {
+class ChartViewActivity : BaseChartActivity() {
 
     //Region attributes
 
-    override val settingsJson: String
-        get() = "./chart_settings.json"
+    override val layoutId = R.layout.screen_chart_view
+
+    override val chartContainerId = R.id.chartContainer
+
+    override val settingsJson = "./chart_settings.json"
 
     override val listener: OnChangeListener
         get() = this
@@ -24,32 +30,32 @@ class ChartViewActivity : BaseChart() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initializeDefaultOptions()
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            when(checkedId) {
-                optionOne.id -> {
-                    listener.onChangeChart(
-                        getTypeByName(optionOne.text.toString())
-                    )
-                }
-                optionTwo.id -> {
-                    listener.onChangeChart(
-                        getTypeByName(optionTwo.text.toString())
-                    )
+        chartSelector.run {
+            firstOptionText = ChartType.BAR.name
+            secondOptionText = ChartType.LINE.name
+            thirdOptionText = ChartType.BUBBLE.name
+            actionListener = object : ChartSelectorActionListener {
+                override fun onChangeAction(checkedId: Int) {
+                    when(checkedId) {
+                        firstOption.id -> {
+                            listener.onChangeChart(
+                                getTypeByName(firstOptionText ?: "")
+                            )
+                        }
+                        secondOption.id -> {
+                            listener.onChangeChart(
+                                getTypeByName(secondOptionText ?: "")
+                            )
 
-                }
-                optionThree.id -> {
-                    listener.onChangeChart(
-                        getTypeByName(optionThree.text.toString())
-                    )
+                        }
+                        thirdOption.id -> {
+                            listener.onChangeChart(
+                                getTypeByName(thirdOptionText ?: "")
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-
-    private fun initializeDefaultOptions() {
-        optionOne.text = ChartType.BAR.name
-        optionTwo.text = ChartType.LINE.name
-        optionThree.text = ChartType.BUBBLE.name
     }
 }
