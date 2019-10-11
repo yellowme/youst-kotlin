74f61aa1c3b1ed0edec551cd0d3e7dcf7581632e
Adrián Leyva S <adrian@yellowme.mx>
2019-10-11 15:53:56 -0500

    Refactor code and customize chart selector

--- playground/src/main/java/mx/yellowme/youst/playground/components/ChartSelector.kt
+++ playground/src/main/java/mx/yellowme/youst/playground/components/ChartSelector.kt
@@ -3,46 +3,28 @@ package mx.yellowme.youst.playground.components
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.component_chart_selector.view.*
 import mx.yellowme.youst.core.extensions.consumeTypeArray
 import mx.yellowme.youst.core.extensions.inflate
+import mx.yellowme.youst.core.utils.loadJsonObject
 import mx.yellowme.youst.playground.R
+import mx.yellowme.youst.playground.domain.ChartSetting
 import mx.yellowme.youst.playground.domain.ChartType
+import mx.yellowme.youst.playground.ui.chart.common.OnChangeListener
 import mx.yellowme.youst.core.R as coreR
 
 /**
  * @author adrianleyvasanchez
  * @since 30,September,2019
  */
-interface ChartSelectorActionListener {
-    fun onChangeAction(checkedType: ChartType)
-}
-
 class ChartSelector @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ) : LinearLayout(context, attrs) {
 
-    var actionListener: ChartSelectorActionListener? = null
-
-    var firstOptionText: String? = null
-        set(value) {
-            field = value
-            firstOption.text = firstOptionText
-        }
-
-    var secondOptionText: String? = null
-        set(value) {
-            field = value
-            secondOption.text = secondOptionText
-        }
-
-    var thirdOptionText: String? = null
-        set(value) {
-            field = value
-            thirdOption.text = thirdOptionText
-        }
+    var listener: OnChangeListener? = null
 
     private var backgroundColorRes: Int = -1
         set(value) {
@@ -66,39 +48,37 @@ class ChartSelector @JvmOverloads constructor(
             }
         }
 
-    fun checkPredefinedOption(id: Int) {
-        when (id) {
-            firstOption.id -> firstOption.isChecked = true
-            secondOption.id -> secondOption.isChecked = true
-            thirdOption.id -> thirdOption.isChecked = true
+    fun setup(activity: AppCompatActivity, listener: OnChangeListener) {
+        this.listener = listener
+        activity.loadJsonObject<ChartSetting>("chart_settings.json")?.let {
+            when(ChartType.valueOf(it.type)) {
+                ChartType.BAR -> firstOption.isChecked = true
+                ChartType.LINE -> secondOption.isChecked = true
+                ChartType.BUBBLE -> thirdOption.isChecked = true
+            }
         }
     }
 
     init {
         inflate(R.layout.component_chart_selector, context)
+        firstOption.text = ChartType.BAR.name
+        secondOption.text = ChartType.LINE.name
+        thirdOption.text = ChartType.BUBBLE.name
         attrs?.consumeTypeArray(context, R.styleable.ChartSelector) {
             backgroundColorRes = getResourceId(R.styleable.ChartSelector_selectorBackgroundColor, coreR.color.white)
             optionsTextColorRes = getResourceId(R.styleable.ChartSelector_selectorOptionsTextColor, coreR.color.guitar_black)
         }
 
-
         radioGroup.setOnCheckedChangeListener { _, checkedId ->
             when(checkedId) {
                 firstOption.id -> {
-                    actionListener?.onChangeAction(
-                        ChartType.valueOf(firstOptionText ?: "")
-                    )
+                    listener?.onChangeChart(ChartType.valueOf(firstOption.text.toString()))
                 }
                 secondOption.id -> {
-                    actionListener?.onChangeAction(
-                        ChartType.valueOf(secondOptionText ?: "")
-                    )
-
+                    listener?.onChangeChart(ChartType.valueOf(secondOption.text.toString()))
                 }
                 thirdOption.id -> {
-                    actionListener?.onChangeAction(
-                        ChartType.valueOf(thirdOptionText ?: "")
-                    )
+                    listener?.onChangeChart(ChartType.valueOf(thirdOption.text.toString()))
                 }
             }
         }
--- playground/src/main/java/mx/yellowme/youst/playground/ui/chart/screens/ChartViewActivity.kt
+++ playground/src/main/java/mx/yellowme/youst/playground/ui/chart/screens/ChartViewActivity.kt
@@ -2,16 +2,11 @@ package mx.yellowme.youst.playground.ui.chart.screens
 
 import android.os.Bundle
 import android.util.Log
-import kotlinx.android.synthetic.main.component_chart_selector.*
 import kotlinx.android.synthetic.main.screen_chart_view.*
-import mx.yellowme.youst.core.utils.loadJsonObject
 import mx.yellowme.youst.playground.R
-import mx.yellowme.youst.playground.components.ChartSelectorActionListener
 import mx.yellowme.youst.playground.data.ChartFakeRepository
 import mx.yellowme.youst.playground.data.SingleItemCallback
 import mx.yellowme.youst.playground.domain.ChartEntry
-import mx.yellowme.youst.playground.domain.ChartSetting
-import mx.yellowme.youst.playground.domain.ChartType
 import mx.yellowme.youst.playground.ui.chart.common.BaseChartActivity
 
 /**
@@ -33,30 +28,11 @@ class ChartViewActivity : BaseChartActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         listener = this
-        chartSelector.run {
-            firstOptionText = ChartType.BAR.name
-            secondOptionText = ChartType.LINE.name
-            thirdOptionText = ChartType.BUBBLE.name
-            checkPredefinedOption(getPredefinedTypeChartId())
-            actionListener = object : ChartSelectorActionListener {
-                override fun onChangeAction(checkedType: ChartType) {
-                    listener?.onChangeChart(checkedType)
-                }
-            }
-        }
+        chartSelector?.setup(this, listener!!)
         repeat(20) {
             getData()
         }
     }
-
-    private fun getPredefinedTypeChartId(): Int =
-        loadJsonObject<ChartSetting>("chart_settings.json")?.let {
-            when(ChartType.valueOf(it.type)) {
-                ChartType.BAR -> firstOption.id
-                ChartType.LINE -> secondOption.id
-                ChartType.BUBBLE -> thirdOption.id
-            }
-        } ?: 0
 
     //TODO: Must delegate action to another layer component (ViewModel or Presenter)
     private fun getData() {
