4797cd903100334bc6edc16d81e7e39d056422a1
Adrián Leyva Sánchez <aleyvaschz@gmail.com>
2019-12-03 15:06:42 -0600

    Implement archie component into challenges

--- challenges/src/main/java/mx/yellowme/youst/challenges/pokemon/PokemonListLoader.kt
+++ challenges/src/main/java/mx/yellowme/youst/challenges/pokemon/PokemonListLoader.kt
@@ -1,7 +1,8 @@
 package mx.yellowme.youst.challenges.pokemon
 
 import mx.yellowme.youst.challenges.common.ChallengeWithListActivity
-import mx.yellowme.youst.core.data.BaseAPIServiceGenerator
+import mx.yellowme.youst.challenges.components.GenericListModel
+import mx.yellowme.youst.challenges.components.GenericListState
 import mx.yellowme.youst.challenges.data.PokemonAPIService
 import mx.yellowme.youst.challenges.data.PokemonAPIServiceGenerator
 import mx.yellowme.youst.challenges.domain.Pokemon
@@ -33,6 +34,27 @@ object PokemonListLoader {
             override fun onFailure(call: Call<MutableList<Pokemon>>, t: Throwable) {
                 view.setState(ChallengeWithListActivity.ListState.LOADED)
                 view.toast("Error")
+            }
+        })
+    }
+
+    fun loadData(view: GenericListModel<Pokemon, PokemonListViewHolder>) {
+        val service = PokemonAPIServiceGenerator().createService(PokemonAPIService::class.java)
+        val call = service.getAll(HashMap())
+
+        view.setState(GenericListState.LOADING)
+        call.enqueue(object : Callback<MutableList<Pokemon>> {
+            override fun onResponse(
+                call: Call<MutableList<Pokemon>>, response: Response<MutableList<Pokemon>>
+            ) {
+                view.setState(GenericListState.LOADED)
+                if (response.isSuccessful) {
+                    view.display(response.body())
+                }
+            }
+
+            override fun onFailure(call: Call<MutableList<Pokemon>>, t: Throwable) {
+                view.setState(GenericListState.LOADED)
             }
         })
     }
